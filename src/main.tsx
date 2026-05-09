ab344ddb8eff003feff879e5e05ee91595eba4c4
Federico <federiconahuellencina@gmail.com>
2026-05-08 23:24:46 -0300

    Revert "unleash integration"
    This reverts commit 534a47bea179353a8689c570f75b3a4f11ca1f63.

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import { FlagProvider } from '@unleash/proxy-client-react';
-
-const config = {
-  url: import.meta.env.VITE_UNLEASH_URL,
-  clientKey: import.meta.env.VITE_UNLEASH_CLIENT_KEY,
-  appName: import.meta.env.VITE_APP_NAME,
-};
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <FlagProvider config={config}>
-      <App />
-    </FlagProvider>
+    <App />
   </React.StrictMode>,
 )

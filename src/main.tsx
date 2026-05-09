a49625288b16e6434196815f725f5a105e7d9dc2
Federico <federiconahuellencina@gmail.com>
2026-05-08 23:02:38 -0300

    unleash integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { FlagProvider } from '@unleash/proxy-client-react';
+
+const config = {
+  url: import.meta.env.VITE_UNLEASH_URL,
+  clientKey: import.meta.env.VITE_UNLEASH_CLIENT_KEY,
+  appName: import.meta.env.VITE_APP_NAME,
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <FlagProvider config={config}>
+      <App />
+    </FlagProvider>
   </React.StrictMode>,
 )
